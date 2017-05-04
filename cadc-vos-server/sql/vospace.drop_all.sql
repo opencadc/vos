7b9bf6a28232ebd5e1f1f670433bf33e7f538101
Alinga Yeung <Alinga.Yeung@nrc-cnrc.gc.ca>
2017-05-04 13:56:27 -0700

    Story 2100. Re-organized sql files.

--- cadc-vos-server/sql/vospace.drop_all.sql
+++
@@ -1,7 +0,0 @@
-
-print "dropping: DeletedNode NodeProperty Node DeletedNodeAvailability NodePropertyAvailability NodeAvailability, "
-drop table DeletedNode;
-drop table NodeProperty;
-drop table Node;
-drop table NodeAvailability;
-
