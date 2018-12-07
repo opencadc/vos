46c4d3c13505a61479e7219eb83b48074a6dec26
Alinga Yeung <Alinga.Yeung@nrc-cnrc.gc.ca>
2018-12-07 10:34:46 -0800

    Story 2346. Add RUNID node property.

--- cadc-vos/src/main/java/ca/nrc/cadc/vos/VOS.java
+++ cadc-vos/src/main/java/ca/nrc/cadc/vos/VOS.java
@@ -235,6 +235,9 @@ public class VOS
      * Standard Node Properties defined by the CADC
      */
 
+    // Pass information to the service, e.g. runid = "TEST" means the service call is a test
+    public static final String PROPERTY_URI_RUNID = "ivo://ivoa.net/vospace/core#runid";
+
     // The size of the resource
     public static final String PROPERTY_URI_CONTENTLENGTH = "ivo://ivoa.net/vospace/core#length";
 
